ddc0d8fd1ec8e43000d42fea526e7820f9498855
Tiago <tiago.polemarxgroup@gmail.com>
2025-10-29 08:54:36 -0300

    Projetos iniciados

---
+++ icompras-servicos/src/main/java/br/com/api/icomprasservicos/IcomprasServicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.icomprasservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcomprasServicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcomprasServicosApplication.class, args);
+    }
+
+}
---
+++ icompras-servicos/src/test/java/br/com/api/icomprasservicos/IcomprasServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.icomprasservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcomprasServicosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
